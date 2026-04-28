1de6074818537341e5b3d78f7a6c856cf7b51207
rakhahasyidan19 <rakhahasyidan19@gmail.com>
2026-04-28 16:37:01 +0700

    pertemuan 9

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -5,8 +5,11 @@ class dashboard extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
+        if (!$this->session->userdata('login')){
+            redirect('login');
+        }
     }
-
+ 
     public function index()
     {
         $data['total_kategori']= $this->db->count_all('kategori');
